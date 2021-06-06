a069441cd81efcc48e1c3d0d07e7efaeb1bfa3bf
Andrej Maier <88maier@mail.ru>
2021-06-06 22:39:26 +0200

    выполнин домашнее задание раздела 4

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+const AVATAR_COUNT = 8;
+const TYPE_HOUSE = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const CHECKINS = ['12:00', '13:00', '14:00'];
+const CHECKOUTS = ['12:00', '13:00', '14:00'];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const PHOTOS = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+const COUNT_OFFER = 10;
+
 function getDecimalRandom (min, max, decimal) {
   if (min < 0 || max < 0) {
     throw 'Я не умею с такие цифрами работать';
@@ -10,76 +18,49 @@ function getDecimalRandom (min, max, decimal) {
   return Number(randomNumber.toFixed(decimal));
 }
 
-getDecimalRandom(10, 50, 5);
-
 function getRandom (min, max) {
   return getDecimalRandom(min, max, 0);
 }
-
-getRandom(5, 70);
-
-
-//Выполнение домашнего задания раздела 4
-
-//const AVATAR_COUNT = 8;
-const TYPE_HOUSE = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-const CHECKINS = ['12:00', '13:00', '14:00'];
-const CHECKOUTS = ['12:00', '13:00', '14:00'];
-//const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-//const PHOTOS = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
 
 function getRandomArrayElement (elements) {
   return elements[getRandom(0, elements.length - 1)];
 }
 
+function getRandomArray(arr) {
+  const countElement = getRandom(1, arr.length);
+  const newArray = [];
+  while (newArray.length < countElement) {
+    const element = getRandomArrayElement(arr);
+    !newArray.includes(element) ? newArray.push(element) : newArray;
+  }
+  return newArray;
+}
+
 function getOffer() {
+  const locationX = getDecimalRandom(35.65000, 35.70000, 5);
+  const locationY = getDecimalRandom(139.70000, 139.80000, 5);
   return {
     author: {
-      //avatar: 'img/avatars/user{{0' + getRandom(1, AVATAR_COUNT) + '}}.png',
+      avatar: `img/avatars/user0${ getRandom(1, AVATAR_COUNT) }.png`,
     },
     offer: {
       title: 'Сдача недвижимости',
-      //address: {{location.x}} {{location.y}},
+      address: `${locationX  }, ${  locationY}`,
       price: getRandom(0, 100000),
       type: getRandomArrayElement(TYPE_HOUSE),
       rooms: getRandom(1, 20),
       guests: getRandom(0, 50),
       checkin: getRandomArrayElement(CHECKINS),
       checkout: getRandomArrayElement(CHECKOUTS),
-      //features: ,
-      //description: ,
-      //photos: ,
+      features: getRandomArray(FEATURES),
+      description: 'Сдаю светлую и уютную однокомнатнаю квартиру на длительный срок от собственника. Этаж - 2. Квартира находится в 5 минутах от метро. Внутри есть все для комфортного проживания: новый диван, шкаф-купе, комод с зеркалом, телевизор, интернет. На кухне  - холодильник, микроволновка, чайник, набор посуды. Санузел раздельный. Ванная комната отделана кафельной плиткой, имется стиральная машинка. Просмотр в любой день. Звоните по телефону: 8 111 111-11-11 (Хозяин).',
+      photos: getRandomArray(PHOTOS),
     },
     location: {
-      lat: getDecimalRandom(35.65000, 35.70000, 5),
-      lng: getDecimalRandom(139.70000, 139.80000, 5),
+      lat: locationX,
+      lng: locationY,
     },
   };
 }
 
-getOffer();
-
-//console.log(getOffer());
-
-// В файле main.js на основе написанных в прошлом задании вспомогательных функций напишите необходимые функции для создания массива из 10 сгенерированных JS-объектов. Каждый объект массива — описание похожего объявления неподалёку.
-
-// Структура каждого объекта должна быть следующей:
-
-//author, объект — описывает автора. Содержит одно поле:
-//  + avatar, строка — адрес изображения вида img/avatars/user{{xx}}.png, где {{xx}} — это число от 1 до 8 с ведущим нулём. Например, 01, 02 и т. д. Адреса изображений не повторяются.
-//offer, объект — содержит информацию об объявлении. Состоит из полей:
-//  title, строка — заголовок предложения. Придумайте самостоятельно.
-//  address, строка — адрес предложения. Для простоты пусть пока составляется из географических координат по маске {{location.x}}, {{location.y}}.
-//  price, число — стоимость. Случайное целое положительное число.
-//  + type, строка — одно из пяти фиксированных значений: palace, flat, house, bungalow или hotel.
-//  rooms, число — количество комнат. Случайное целое положительное число.
-//  guests, число — количество гостей, которое можно разместить. Случайное целое положительное число.
-//  + checkin, строка — одно из трёх фиксированных значений: 12:00, 13:00 или 14:00.
-//  + checkout, строка — одно из трёх фиксированных значений: 12:00, 13:00 или 14:00.
-//  + features, массив строк — массив случайной длины из значений: wifi, dishwasher, parking, washer, elevator, conditioner. Значения не должны повторяться.
-//  description, строка — описание помещения. Придумайте самостоятельно.
-//  + photos, массив строк — массив случайной длины из значений: https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg, https://assets.htmlacademy.ru/content///intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg, https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg.
-//location, объект — местоположение в виде географических координат. Состоит из двух полей:
-//  lat, число с плавающей точкой — широта, случайное значение от 35.65000 до 35.70000.
-//  lng, число с плавающей точкой — долгота, случайное значение от 139.70000 до 139.80000.
-
+const offers = new Array(COUNT_OFFER).fill().map(() => getOffer());
