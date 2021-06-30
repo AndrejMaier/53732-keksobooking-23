98e745f86f7f46608cabe3966bf73e0c8e44cdd4
Andrej Maier <88maier@mail.ru>
2021-06-30 12:16:31 +0200

    Создал функцию по проверки отсутствия значений у объявлении

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-import {generateOffers} from './moki/data.js';
-import {renderOffers} from './moki/similar-offers-list.js';
+import {generateOffers} from './mocks/data.js';
+import {renderOffers} from './similar-offers-list.js';
 const COUNT_OFFER = 1;
 
 renderOffers(generateOffers(COUNT_OFFER));
