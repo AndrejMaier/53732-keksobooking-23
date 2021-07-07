0e0b381ec6574726c939af5b8f0ae09a100dbca6
Andrej Maier <88maier@mail.ru>
2021-07-07 10:19:35 +0200

    выполнил задание 8.2

--- js/form.js
+++ js/form.js
@@ -11,7 +11,7 @@ const rooms = document.querySelector('#room_number');
 const quantityGuests = document.querySelector('#capacity');
 const quantityGuestsList = quantityGuests.querySelectorAll('option');
 
-
+// Переводит страницу в неактивное состояние
 const disableForm = () => {
   adForm.classList.add('ad-form--disabled');
   adFormFieldsets.forEach((element) => {
@@ -26,6 +26,7 @@ const disableForm = () => {
   });
 };
 
+// Переводит страницу в активное состояние
 const enableForm = () => {
   adForm.classList.remove('ad-form--disabled');
   adFormFieldsets.forEach((element) => {
@@ -44,6 +45,7 @@ const enableForm = () => {
 // Валидация формы
 //
 
+// синхронизация полей "количество комнат" и "количество мест"
 const syncRoomsAndGuests = () => {
   rooms.addEventListener('change', (e) => {
     const room = Number(e.target.value);
@@ -58,6 +60,7 @@ const syncRoomsAndGuests = () => {
   });
 };
 
+// синхронизация полей "тип жилья" и "цена за ночь"
 const syncTypeHouseAndPrice = () => {
   typeHouse.addEventListener('change', (e) => {
     priceField.min = typeHouseAndPrice[e.target.value];
@@ -70,6 +73,7 @@ const checkinFieldList = checkinField.querySelectorAll('option');
 const checkoutField = document.querySelector('#timeout');
 const checkoutFieldList = checkoutField.querySelectorAll('option');
 
+// обработчик события изменения значения полей
 const syncCheckTime = (time1, time2) => {
   time1.addEventListener('change', (e) => {
     time2.forEach((option) => {
@@ -80,10 +84,18 @@ const syncCheckTime = (time1, time2) => {
   });
 };
 
+//синхронизация полей "время заезда и выезда"
 const syncCheckinAndCheckout = () => {
   syncCheckTime(checkinField, checkoutFieldList);
   syncCheckTime(checkoutField, checkinFieldList);
 };
 
+// настройка формы
+const initForm = () => {
+  syncRoomsAndGuests();
+  syncTypeHouseAndPrice();
+  syncCheckinAndCheckout();
+};
 
-export {disableForm, enableForm, syncRoomsAndGuests, syncTypeHouseAndPrice, syncCheckinAndCheckout};
+
+export {disableForm, enableForm, initForm};
